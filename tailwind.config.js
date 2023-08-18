32b5092fa2bbc2f8af30abaa2b627173a8a4abe1
Abolfazl Rahmati <rahmati.business@gamil.com>
2023-08-18 23:21:23 +0330

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html" , " ./src/js/theme.js " ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
